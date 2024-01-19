8042ac792aa9439eb3f03f610bdacdd23851951a
Sean Carter <sacarter@gmail.com>
2024-01-19 07:40:28 +0000

    reinstated layout change...again

--- src/Project/Sugcon/SugconEuSxa/src/Layout.tsx
+++ src/Project/Sugcon/SugconEuSxa/src/Layout.tsx
@@ -43,8 +43,8 @@ const Layout = ({ layoutData }: LayoutProps): JSX.Element => {
       <Head>
         <title>
           {fields?.Title?.value
-            ? 'SUGCON Europe - ' + fields.Title.value.toString()
-            : 'SUGCON Europe'}
+            ? 'SC Demo - ' + fields.Title.value.toString()
+            : 'SC Demo'}
         </title>
         <link rel="icon" href={`${publicUrl}/favicon.ico`} />
         {/* Meta Tags */}
